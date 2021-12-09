287cfae91a8d7fe5be25688d6b9a923b776397fe
enwoode <95760908+enwoode@users.noreply.github.com>
2021-12-09 13:39:35 +0000

    Add files via upload

---
+++ data/OsmFile_0.js
@@ -0,0 +1,13 @@
+var json_OsmFile_0 = {
+"type": "FeatureCollection",
+"name": "OsmFile_0",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "full_id": "n4229940994", "osm_id": "4229940994", "osm_type": "node", "entrance": "yes", "addr:street": "", "name": "", "opening_hours": "", "tourism": "", "shop": "", "power": "", "place": "" }, "geometry": { "type": "Point", "coordinates": [ -2.4945001, 6.2004161 ] } },
+{ "type": "Feature", "properties": { "full_id": "n8103253617", "osm_id": "8103253617", "osm_type": "node", "entrance": "", "addr:street": "Heaven View Street, New Adiembra - Sefwi Wiawso", "name": "Heaven View Hotel", "opening_hours": "24\/7", "tourism": "hotel", "shop": "", "power": "", "place": "" }, "geometry": { "type": "Point", "coordinates": [ -2.5011063, 6.2025514 ] } },
+{ "type": "Feature", "properties": { "full_id": "n9231193050", "osm_id": "9231193050", "osm_type": "node", "entrance": "", "addr:street": "", "name": "", "opening_hours": "", "tourism": "", "shop": "convenience", "power": "", "place": "" }, "geometry": { "type": "Point", "coordinates": [ -2.4943518, 6.195121 ] } },
+{ "type": "Feature", "properties": { "full_id": "n9234262476", "osm_id": "9234262476", "osm_type": "node", "entrance": "", "addr:street": "", "name": "", "opening_hours": "", "tourism": "", "shop": "", "power": "transformer", "place": "" }, "geometry": { "type": "Point", "coordinates": [ -2.493931, 6.1948708 ] } },
+{ "type": "Feature", "properties": { "full_id": "n9244806956", "osm_id": "9244806956", "osm_type": "node", "entrance": "", "addr:street": "", "name": "Adienbra New Town", "opening_hours": "", "tourism": "", "shop": "", "power": "", "place": "suburb" }, "geometry": { "type": "Point", "coordinates": [ -2.4957142, 6.1995814 ] } },
+{ "type": "Feature", "properties": { "full_id": "n9247350859", "osm_id": "9247350859", "osm_type": "node", "entrance": "", "addr:street": "", "name": "", "opening_hours": "", "tourism": "", "shop": "convenience", "power": "", "place": "" }, "geometry": { "type": "Point", "coordinates": [ -2.4948834, 6.2006675 ] } }
+]
+}
---
+++ data/addrstreet_4.js
@@ -0,0 +1,8 @@
+var json_addrstreet_4 = {
+"type": "FeatureCollection",
+"name": "addrstreet_4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "full_id": "n8103253617", "osm_id": "8103253617", "osm_type": "node", "addr:street": "Heaven View Street, New Adiembra - Sefwi Wiawso", "name": "Heaven View Hotel", "opening_hours": "24\/7", "tourism": "hotel" }, "geometry": { "type": "Point", "coordinates": [ -2.5011063, 6.2025514 ] } }
+]
+}
